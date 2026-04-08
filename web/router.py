53d49285efed563089942c8abe5f9af28096a9ef
dev-cdy <argon4067@gmail.com>
2026-04-08 21:05:29 +0900

    접속관련

--- web/router.py
+++ web/router.py
@@ -7,15 +7,15 @@ web_router = APIRouter()
 # 메인 및 서비스 안내 페이지
 @web_router.get("/")
 async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="index.html")
 
 @web_router.get("/service-intro")
 async def service_intro(request: Request):
-    return templates.TemplateResponse("service_intro.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="service_intro.html")
 
 @web_router.get("/how-to-use")
 async def how_to_use(request: Request):
-    return templates.TemplateResponse("how_to_use.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="how_to_use.html")
 
 # 도메인별 분리된 라우터 포함
 web_router.include_router(auth.router)
